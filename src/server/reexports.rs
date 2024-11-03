bdf0dd4ffe73a768dd62014df551b0e568d84540
Stefan Terdell <stefan.terdell+git@proton.me>
2024-11-03 12:38:00 +0100

    Rename some more features, add some reexports

--- src/server/reexports.rs
+++ src/server/reexports.rs
@@ -1,7 +1,18 @@
+#[cfg(feature = "server-oauth-callbacks")]
+pub use anyhow;
+#[cfg(feature = "server-pages")]
+pub use askama;
 pub use async_trait;
+#[cfg(feature = "server-oauth-callbacks")]
 pub use base64;
+#[cfg(feature = "server-email")]
+pub use lettre;
 #[cfg(feature = "server-oauth-callbacks")]
 pub use oauth2;
+#[cfg(feature = "server-password")]
+pub use password_auth;
+#[cfg(any(feature = "server-email", feature = "server-oauth-callbacks"))]
+pub use reqwest;
 pub use tokio;
 #[cfg(any(feature = "server-email", feature = "server-oauth-callbacks"))]
 pub use url;
